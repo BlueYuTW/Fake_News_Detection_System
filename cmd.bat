f54350b8349ce46b7c94f58ebc09abc9640ee41f
藍瑜 <116458212+BlueYu1201@users.noreply.github.com>
2025-12-23 15:59:53 +0800

    yes

--- cmd.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start cmd /k ngrok http http://localhost:8080
--- start_update.bat
+++ start_update.bat
@@ -1,4 +1,3 @@
 @echo off
 cd /d "%~dp0"
-:: 檢查 PHP 是否在環境變數中，如果沒有，請將下方的 php 改成完整路徑 (例如 C:\xampp\php\php.exe)
 php update_hot_topics.php
